53dce1c30358eab96cfbaa8ca563e7bb9234900e
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-03-22 18:55:51 +0100

    feat: completed api

--- main.go
+++ main.go
@@ -51,6 +51,24 @@ func getTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
+func toggleStatus(context *gin.Context) {
+	id := context.Param("id")
+
+	fmt.Println(id, "id")
+
+	todo, err := getTodosById(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{
+			"message": "Todo not found",
+		})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func getTodosById(id string) (*todo, error) {
 	for i, t := range todos {
 		if t.ID == id {
@@ -58,13 +76,14 @@ func getTodosById(id string) (*todo, error) {
 		}
 	}
 
-	return nil, errors.New("Todo not found")
+	return nil, errors.New("todo not found")
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleStatus)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
